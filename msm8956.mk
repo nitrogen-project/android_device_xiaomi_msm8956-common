7e29d48ca537ab375307ad0d2ee1f7d5ce6a62cf
Rk779 <Rahulkrishna585@gmail.com>
2018-04-07 14:03:53 +0000

    msm8956-common: Add thermal HAL from CAF
    Change-Id: Ib450e4354045cdb6b5dad77f6dbe83409924bfb5

--- msm8956.mk
+++ msm8956.mk
@@ -313,6 +313,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8952
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
